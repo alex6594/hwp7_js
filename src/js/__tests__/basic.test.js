e5ba6a8a24d81c6ea426f04e4828d2c68b15db86
unknown <alex6594@mail.ru>
2023-03-29 18:25:44 +0300

    change tests

--- src/js/__tests__/basic.test.js
+++ src/js/__tests__/basic.test.js
@@ -1,18 +1,31 @@
 import Validator from '../basic';
 
 test('validateUsername method exists and works correctly', () => {
-  expect(Boolean(Validator.validateUsername)).toBeTruthy();
-  expect(Validator.validateUsername('aBc123-456xYz')).toBeTruthy();
-  expect(Validator.validateUsername('gIj78_90uVw')).toBeTruthy();
-  expect(Validator.validateUsername('-name')).toBeFalsy();
-  expect(Validator.validateUsername('name-')).toBeFalsy();
-  expect(Validator.validateUsername('_name')).toBeFalsy();
-  expect(Validator.validateUsername('name_')).toBeFalsy();
-  expect(Validator.validateUsername('1name')).toBeFalsy();
-  expect(Validator.validateUsername('name1')).toBeFalsy();
-  expect(Validator.validateUsername('na1234me')).toBeFalsy();
-  expect(Validator.validateUsername('na.me')).toBeFalsy();
-  expect(Validator.validateUsername('na!me')).toBeFalsy();
-  expect(Validator.validateUsername('na[me')).toBeFalsy();
-  expect(Validator.validateUsername('na|me')).toBeFalsy();
-});
+  expect(Boolean(Validator.validateUsername)).toBeTruthy()})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('aBc123-456xYz')).toBeTruthy()})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('gIj78_90uVw')).toBeTruthy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('-name')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('name-')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('_name')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('name_')).toBeFalsy()});
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('1name')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('name1')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('na1234me')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('na.me')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('na!me')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('na[me')).toBeFalsy();})
+test('validateUsername method exists and works correctly', () => {
+  expect(Validator.validateUsername('na|me')).toBeFalsy();})
+
